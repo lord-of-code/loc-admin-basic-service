e068005e5473defa96a611438efddfd23db7bbd4
longzhe <529039038@qq.com>
2018-04-07 17:50:53 +0800

    初始化项目

---
+++ src/main/java/com/loc/admin/basic/Application.java
@@ -0,0 +1,15 @@
+package com.loc.admin.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created on 2018/4/7.
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
